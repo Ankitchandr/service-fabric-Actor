28ed38de52d2fa7c4ad940805aef81bfa1e9c357
ankit chandra <ankitchandra1994@gmail.com>
2018-04-06 13:06:12 +0530

    Service Fabric Actor Console Application

---
+++ MyApplication/ActorClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWorld.Interfaces;
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWorld actor = ActorProxy.Create<IHelloWorld>(ActorId.CreateRandom(), new Uri("fabric:/MyApplication/HelloWorldActorService"));
+            Task<string> retval = actor.GetHelloWorldAsync();
+            Console.WriteLine(retval.Result);
+            
+            Task<string> retval1 = actor.GetHelloAsync();
+            Console.Write(retval1.Result);
+            Console.ReadLine();
+        }
+    }
+}
